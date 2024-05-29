ad0d5de4401d92ca698cc973c1c2ab93569fcc97
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2024-05-29 18:40:33 -0400

    Add 06-Points

---
+++ 06-Points/shaders.js
@@ -0,0 +1,55 @@
+let newFlickering = new ShaderProgram("new-flickering-dots");
+
+newFlickering.vertText = `
+    // beginGLSL
+    attribute vec4 coordinates;
+    varying vec2 myposition;
+    varying vec2 center;
+    varying float alph;
+    void main(void) {
+        gl_Position = vec4(coordinates.x, coordinates.y, 0.0, 1.0);
+        center = vec2(gl_Position.x, gl_Position.y);
+        center = 512.0 + center * 512.0;
+        myposition = vec2(gl_Position.x, gl_Position.y);
+        alph = coordinates.w;
+        gl_PointSize = (9. + coordinates.z / ((6.0 + alph) * 0.25)) * 1.0;
+    }
+    // endGLSL
+`;
+newFlickering.fragText = `
+    // beginGLSL
+    precision mediump float;
+    varying vec2 myposition;
+    varying vec2 center;
+    varying float alph;
+    float rand(vec2 co){
+        return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453 * (2.0 + sin(co.x)));
+    }
+    void main(void) {
+        // vec2 uv = gl_PointCoord.xy / vec2(1600, 1600);
+        // float d = length(uv - center);
+        // vec2 pos = myposition;
+        vec2 uv = gl_FragCoord.xy / vec2(2560, 1600);
+        // uv.x = uv.x + 1.0;
+        uv = uv * 2.0;
+        uv = uv + 0.5;
+        // uv = uv * 1.0;
+        float ALPHA = 0.75;
+        vec2 pos = gl_PointCoord - vec2(0.5, 0.5);
+                float dist_squared = dot(pos, pos);
+        float alpha;
+        if (dist_squared < 0.25) {
+            alpha = ALPHA;
+        } else {
+            alpha = 0.0;
+        }
+        alpha = smoothstep(0.015 / (0.9 + alph), 0.000125, dist_squared) * 0.49;
+        float rando = rand(pos);
+        // gl_FragColor = vec4(1.0, (1.0 - dist_squared * 40.) * 0.6, 0.0, alpha + ((0.12 - dist_squared) * 4.) - (rando * 0.2));
+        gl_FragColor = vec4(1.0, 0.2 - dist_squared, 0.0 + alpha * 120., ((3. - dist_squared * 24.0 * (0.25 + alph) - (rando * 1.1)) * 0.045 + alpha)) * 1.25;
+        gl_FragColor.rgb = gl_FragColor.rrr;
+        
+    }
+    // endGLSL
+`;
+newFlickering.init();
---
+++ 06-Points/sketch.js
@@ -0,0 +1,115 @@
+let looping = true;
+let keysActive = true;
+let socket, cnvs, gl, shaderProgram, time, resolution;
+let drawCount = 0, drawIncrement = 1;
+let vertices, vbuffer, dotsVBuf;
+let currentProgram;
+let ratio;
+const seed = 10;
+const openSimplex = openSimplexNoise(seed);
+
+function setup() {
+    socket = io.connect('http://localhost:8080');
+    pixelDensity(1);
+    noCanvas();
+    cnvs = document.getElementById('cnvs');
+    cnvs.width = window.innerWidth;
+    cnvs.height = window.innerHeight;
+    gl = cnvs.getContext('webgl', { preserveDrawingBuffer: true });
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthMask(false);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.enable(gl.BLEND);
+    gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
+    gl.viewport(0, 0, cnvs.width, cnvs.height);
+    vbuffer = gl.createBuffer();
+    frameRate(20);
+    dotsVBuf = gl.createBuffer();
+    shadersReadyToInitiate = true;
+    initializeShaders();
+    currentProgram = getProgram("new-flickering-dots");
+    gl.useProgram(currentProgram);
+    setTimeout(function() {
+        scdConsoleArea.setAttribute("style", "display:block;");
+        scdArea.style.display = "none";
+        scdConsoleArea.setAttribute("style", "display:none;");
+        jsCmArea.style.height = "685px";
+        jsArea.style.display = "block";
+        displayMode = "js";
+        javaScriptEditor.cm.refresh();
+    }, 1);
+    ratio = window.innerHeight / window.innerWidth; 
+    if (!looping) {
+        noLoop();
+    }
+}
+
+draw = function() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    drawSpiral(currentProgram);
+    // drawSimplexNoiseField(currentProgram);
+    drawCount += drawIncrement;
+}
+
+drawSpiral = function(selectedProgram) {
+    vertices = [];
+    let t = drawCount * 1e-4;
+    for (let i = 0; i < 27000; i += 1) {
+        let x = Math.cos(i * t) * i * 5e-5;
+        let y = Math.sin(i * t) * i * 5e-5;
+        vertices.push(x * ratio, y, 15, 0.3);
+    }
+    /*======== Associating shaders to buffer objects ========*/
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ARRAY_BUFFER, dotsVBuf);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    // Get the attribute location
+    var coord = gl.getAttribLocation(selectedProgram, "coordinates");
+    // Point an attribute to the currently bound VBO
+    gl.vertexAttribPointer(coord, 4, gl.FLOAT, false, 0, 0);
+    // Enable the attribute
+    gl.enableVertexAttribArray(coord);
+    /*============= Drawing the primitive ===============*/
+    gl.drawArrays(gl.POINTS, 0, 27000);
+}
+
+
+drawSimplexNoiseField = function(selectedProgram) {
+    vertices = [];
+    let t = drawCount * 0.5e-1;
+    let dotAmount = 30000;
+    dotAmount = Math.sqrt(dotAmount);
+    for (let x = 0; x < dotAmount; x++) {
+        for (let y = 0; y < (dotAmount * ratio); y++) {
+           let n = (openSimplex.noise3D(x * 0.1, y * 0.1, t) + 1) * 0.5;
+           vertices.push((x / dotAmount - 0.5) * 1.95, (y / dotAmount / ratio - 0.5) * 1.95, 50 * n, 0.3);
+        }
+    }
+    /*======== Associating shaders to buffer objects ========*/
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.bindBuffer(gl.ARRAY_BUFFER, dotsVBuf);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    // Get the attribute location
+    var coord = gl.getAttribLocation(selectedProgram, "coordinates");
+    // Point an attribute to the currently bound VBO
+    gl.vertexAttribPointer(coord, 4, gl.FLOAT, false, 0, 0);
+    // Enable the attribute
+    gl.enableVertexAttribArray(coord);
+    /*============= Drawing the primitive ===============*/
+    gl.drawArrays(gl.POINTS, 0, vertices.length / 4);
+}
+
+function keyPressed() {
+    if (keysActive) {
+        if (keyCode === 32) {
+            if (looping) {
+                noLoop();
+                looping = false;
+            } else {
+                loop();
+                looping = true;
+            }
+        }
+    }
+}
